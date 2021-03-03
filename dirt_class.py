0381392ef7167d247c1c9467047ba4985ffe9cee
Wil-conn <wilconnolly@cmail.carleton.ca>
2021-03-02 20:47:01 -0500

    tweeked numbers in dirt class

--- dirt_class.py
+++ dirt_class.py
@@ -13,12 +13,17 @@ class dirt:
     #for the dirt block it goes through every element of its environment, if there is a grass block in its environment then there is a 20% chance said dirt block will become a grass block
     def cycle(self, environment):
         x = random.rand() * 100
+        multiplyer = 1
+        c = 0
         for rows in environment:
             for element in rows:
                 if isinstance(element, grass):
-                    if x < 20:
+                    c += 1
+                    if c == 4:
                         return self.turn_to_grass()
-        if x < 0.1:
+                    elif x < 2:
+                        return self.turn_to_grass()
+        if x < 0.02:
             return self.turn_to_grass()
     #creates a new grass object and returns to the gameboard where this new object should be places
     def turn_to_grass(self):
